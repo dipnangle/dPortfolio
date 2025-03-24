84e22469cdde0bc8b4b7cd4921cd54b9be9c09dc
dipnangle <dipnangle@gmail.com>
2025-03-24 16:49:23 +0530

    make changes for production branch

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -23,49 +23,6 @@ const Tooltip = ({ children, tooltipTitle, tooltipText, show, position = "top", 
 
             {/* Tooltip */}
             {(show || children) && (
-                // <div className={`w-full absolute invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all duration-300 ease-out transform group-hover:translate-y-0 translate-y-2 ${positionClasses[position]}`}>
-                //     <div className="relative w-full p-4 bg-gradient-to-br from-gray-900/95 to-gray-800/95 backdrop-blur-md rounded-2xl border border-white/10 shadow-[0_0_30px_rgba(79,70,229,0.15)]">
-                //         <div className="flex items-center gap-3 mb-2">
-                //             <div className="flex items-center w-full justify-center rounded-full bg-indigo-500/20">
-                //                 <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 text-indigo-400">
-                //                     <path
-                //                         clipRule="evenodd"
-                //                         d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                //                         fillRule="evenodd"
-                //                     ></path>
-                //                 </svg>
-                //             </div>
-                //             <h3 className="text-sm font-semibold text-white">{tooltipTitle}</h3>
-                //         </div>
-                //         <p className="text-sm text-gray-300">{tooltipText}</p>
-                //         <div className="absolute inset-0 rounded-2xl bg-gradient-to-r from-indigo-500/10 to-purple-500/10 blur-xl opacity-50"></div>
-
-                //         {/* Arrow */}
-                //         <div className={`absolute w-3 h-3 bg-gradient-to-br from-gray-900/95 to-gray-800/95 border-r border-b border-white/10 ${arrowPositionClasses[position]}`}></div>
-                //     </div>
-                // </div>
-                // <div className={`absolute  invisible opacity-0 group-hover:visible group-hover:opacity-100 ${positionClasses[position]} mb-3 w-60 transition-all duration-300 ease-out transform group-hover:translate-y-0 translate-y-2`}>
-                //     <div className="relative p-2 bg-gradient-to-br from-gray-900/95 to-gray-800/95 backdrop-blur-md rounded-2xl border border-white/10 shadow-[0_0_30px_rgba(79,70,229,0.15)]">
-                //         <div className="flex items-center gap-3">
-                //             <div className="flex items-center justify-center w-9 h-9 rounded-full bg-indigo-500/20 text-sm">
-                //                 <i className={`${icon} text-blue-500 dark:text-indigo-300`}></i>
-                //                 {/* <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 text-indigo-400">
-                //                     <path clip-rule="evenodd"
-                //                         d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                //                         fill-rule="evenodd"></path>
-                //                 </svg> */}
-                //             </div>
-                //             <h3 className="text-sm font-semibold text-white">{tooltipTitle}<span className="text-blue-600 ml-1 text-lg">...</span></h3>
-                //         </div>
-                //         <div className="space-y-2">
-                //             <p className="text-sm text-gray-300">
-                //                 {tooltipText}
-                //             </p>
-                //         </div>
-                //         <div className="absolute inset-0 rounded-2xl bg-gradient-to-r from-indigo-500/10 to-purple-500/10 blur-xl opacity-50"></div>
-                //         <div className={`absolute ${arrowPositionClasses[position]} w-3 h-3 bg-gradient-to-br from-gray-900/95 to-gray-800/95 border-r border-b border-white/10`}></div>
-                //     </div>
-                // </div>
                 <div className={`absolute invisible opacity-0 group-hover:visible group-hover:opacity-100 ${positionClasses[position]} mb-3 w-60 transition-all duration-300 ease-out transform group-hover:translate-y-0 translate-y-2`}>
                     <div className="relative p-2 bg-white dark:bg-gray-900 backdrop-blur-md rounded-2xl border border-gray-300 dark:border-white/10 shadow-lg dark:shadow-[0_0_30px_rgba(79,70,229,0.15)]">
                         <div className="flex items-center gap-3">
